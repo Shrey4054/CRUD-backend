7c34cd3c2031e97a92c4edf94dcc7a9df9f72d13
Shrey4054 <shrey4054@gmail.com>
2025-08-23 15:10:56 -0400

    change env

--- db.js
+++ db.js
@@ -2,11 +2,8 @@ const { Pool} = require('pg')
 require('dotenv').config()
 
 const pool =  new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT)
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized:false}
 })
 
 
