c33538c21517cc34b4c60e9bc8f9de27f194ec78
Shrey4054 <shrey4054@gmail.com>
2025-09-04 13:34:00 -0400

    origins

--- server.js
+++ server.js
@@ -9,12 +9,18 @@ const AuthRoutes = require('./routes/Auth')
 const taskListRoutes = require('./routes/taskListApi')
 const redirectRoutes = require('./middlewares/redirects')
 
+const allowedOrigins = [
+    "https://crud-frontend-5lt9.onrender.com",
+    "https://raygunoxc.xyz"
+];
+
 const app = express()
 const PORT = process.env.PORT || 3000
 const SECRET = process.env.SECRET || "dfhjsjakdfhsaot4uhfjdao;u4tsjai"
 
 app.use(cors(
     {
+        origin: allowedOrigins,
         credentials: true
     }
 ))
