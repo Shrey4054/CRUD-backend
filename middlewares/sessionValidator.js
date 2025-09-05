0b2033970654e086a5a9d4d8300adf7fcd2f3e61
Shrey4054 <shrey4054@gmail.com>
2025-09-05 12:46:45 -0400

    commit

--- middlewares/sessionValidator.js
+++ middlewares/sessionValidator.js
@@ -1,8 +1,8 @@
 function sessionValidator(req,res,next){
-    console.log(`Before if else: ${res.user}`) //undefined why
+    console.log(`Before if else: ${res.session}`) //undefined why
     const openPath  = ['/','/signup.css','/clientAuth.js', "/Auth/action","/Auth/action/register","/Auth/action/login"]
     if(openPath.includes(req.path)) return next()
-    if(req.session.user && req.session.user.id){
+    if(req.session.user){
         next()
     }else{
         console.log(`inside the else:${req.session.user}`) //undefined why
